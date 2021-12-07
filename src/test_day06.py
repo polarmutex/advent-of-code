e71890cf8fbfdf65a882a0a8f82d5a721f51600e
Brian Ryall <brian@brianryall.xyz>
2021-12-06 23:56:52 -0500

    chore: refactor

--- src/test_day06.py
+++ src/test_day06.py
@@ -1,20 +1,34 @@
 import pytest
 
-from src.day06 import INPUT_S, compute, parse
+from src.day06 import INPUT_S, parse, INPUT_TXT, part1, part2
 
 
 @pytest.fixture
 def example():
-    return parse(INPUT_S)
+    return INPUT_S
+
+
+@pytest.fixture
+def input():
+    with open(INPUT_TXT) as f:
+        return f.read()
 
 
 def test_parse(example):
-    assert example == [3, 4, 3, 1, 2]
+    assert parse(example) == [3, 4, 3, 1, 2]
 
 
-def test_part1(example) -> None:
-    assert compute(example, 80) == 5934
+def test_part1_example(example) -> None:
+    assert part1(example) == 5934
 
 
-def test_part2(example) -> None:
-    assert compute(example, 256) == 26984457539
+def test_part2_example(example) -> None:
+    assert part2(example) == 26984457539
+
+
+def test_part1_input(input) -> None:
+    assert part1(input) == 388739
+
+
+def test_part2_input(input) -> None:
+    assert part2(input) == 1741362314973
