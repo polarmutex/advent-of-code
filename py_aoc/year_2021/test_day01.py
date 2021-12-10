df0fd9cf0d22edbbf5cf270b2c4e5a3cfd0e2653
Brian Ryall <brian@brianryall.xyz>
2021-12-09 23:48:59 -0500

    fix: update day1 with other sources

--- py_aoc/year_2021/test_day01.py
+++ py_aoc/year_2021/test_day01.py
@@ -1,6 +1,6 @@
 import pytest
 
-from py_aoc.year_2021.day01 import INPUT_S, INPUT_GITHUB, compute, parse
+from py_aoc.year_2021.day01 import INPUT_S, INPUT_GITHUB, INPUT_GOOGLE, INPUT_REDDIT, INPUT_TWITTER, compute, parse
 
 
 @pytest.fixture
@@ -11,6 +11,24 @@ def example():
 @pytest.fixture
 def github():
     with open(INPUT_GITHUB) as f:
+        return parse(f.read())
+
+
+@pytest.fixture
+def google():
+    with open(INPUT_GOOGLE) as f:
+        return parse(f.read())
+
+
+@pytest.fixture
+def twitter():
+    with open(INPUT_TWITTER) as f:
+        return parse(f.read())
+
+
+@pytest.fixture
+def reddit():
+    with open(INPUT_REDDIT) as f:
         return parse(f.read())
 
 
@@ -26,9 +44,33 @@ def test_part1_github(github) -> None:
     assert compute(github, 1) == 1448
 
 
+def test_part1_google(google) -> None:
+    assert compute(google, 1) == 1121
+
+
+def test_part1_twitter(twitter) -> None:
+    assert compute(twitter, 1) == 1557
+
+
+def test_part1_reddit(reddit) -> None:
+    assert compute(reddit, 1) == 1583
+
+
 def test_part2_example(example) -> None:
     assert compute(example, 3) == 5
 
 
 def test_part2_github(github) -> None:
     assert compute(github, 3) == 1471
+
+
+def test_part2_google(google) -> None:
+    assert compute(google, 3) == 1065
+
+
+def test_part2_twitter(twitter) -> None:
+    assert compute(twitter, 3) == 1608
+
+
+def test_part2_reddit(reddit) -> None:
+    assert compute(reddit, 3) == 1627
