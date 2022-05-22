4c2b2388aace4a8db2b7a26ad4b474f18578fbfa
Brian Ryall <brian@brianryall.xyz>
2022-05-22 00:52:31 -0400

    feat: 2020 day1

--- README.md
+++ README.md
@@ -42,36 +42,36 @@
 
 ## 2020
 
-![Alt](https://img.shields.io/badge/stars%20⭐-0-yellow)
-![Alt](https://img.shields.io/badge/days%20completed-0-red)
+![Alt](https://img.shields.io/badge/stars%20⭐-2-yellow)
+![Alt](https://img.shields.io/badge/days%20completed-1-red)
 
-|                      Day                       |          Name           | Stars |
-| :--------------------------------------------: | :---------------------: | :---: |
-|  [Day 1](https://adventofcode.com/2020/day/1)  |      Report Repair      |       |
-|  [Day 2](https://adventofcode.com/2020/day/2)  |   Password Philosophy   |       |
-|  [Day 3](https://adventofcode.com/2020/day/3)  |   Toboggan Trajectory   |       |
-|  [Day 4](https://adventofcode.com/2020/day/4)  |   Passport Processing   |       |
-|  [Day 5](https://adventofcode.com/2020/day/5)  |     Binary Boarding     |       |
-|  [Day 6](https://adventofcode.com/2020/day/6)  |     Custom Customs      |       |
-|  [Day 7](https://adventofcode.com/2020/day/7)  |    Handy Haversacks     |       |
-|  [Day 8](https://adventofcode.com/2020/day/8)  |    Handheld Halting     |       |
-|  [Day 9](https://adventofcode.com/2020/day/9)  |     Encoding Error      |       |
-| [Day 10](https://adventofcode.com/2020/day/10) |      Adapter Array      |       |
-| [Day 11](https://adventofcode.com/2020/day/11) |     Seating System      |       |
-| [Day 12](https://adventofcode.com/2020/day/12) |        Rain Risk        |       |
-| [Day 13](https://adventofcode.com/2020/day/13) |     Shuttle Search      |       |
-| [Day 14](https://adventofcode.com/2020/day/14) |      Docking Data       |       |
-| [Day 15](https://adventofcode.com/2020/day/15) | Rambunctious Recitation |       |
-| [Day 16](https://adventofcode.com/2020/day/16) |   Ticket Translation    |       |
-| [Day 17](https://adventofcode.com/2020/day/17) |      Conway Cubes       |       |
-| [Day 18](https://adventofcode.com/2020/day/18) |     Operation Order     |       |
-| [Day 19](https://adventofcode.com/2020/day/19) |    Monster Messages     |       |
-| [Day 20](https://adventofcode.com/2020/day/20) |     Jurassic Jigsaw     |       |
-| [Day 21](https://adventofcode.com/2020/day/21) |   Allergen Assessment   |       |
-| [Day 22](https://adventofcode.com/2020/day/22) |       Crab Combat       |       |
-| [Day 23](https://adventofcode.com/2020/day/23) |        Crab Cups        |       |
-| [Day 24](https://adventofcode.com/2020/day/24) |      Lobby Layout       |       |
-| [Day 25](https://adventofcode.com/2020/day/25) |      Combo Breaker      |       |
+|                      Day                       |          Name           |     Stars     |
+| :--------------------------------------------: | :---------------------: | :-----------: |
+|  [Day 1](https://adventofcode.com/2020/day/1)  |      Report Repair      | :star: :star: |
+|  [Day 2](https://adventofcode.com/2020/day/2)  |   Password Philosophy   |               |
+|  [Day 3](https://adventofcode.com/2020/day/3)  |   Toboggan Trajectory   |               |
+|  [Day 4](https://adventofcode.com/2020/day/4)  |   Passport Processing   |               |
+|  [Day 5](https://adventofcode.com/2020/day/5)  |     Binary Boarding     |               |
+|  [Day 6](https://adventofcode.com/2020/day/6)  |     Custom Customs      |               |
+|  [Day 7](https://adventofcode.com/2020/day/7)  |    Handy Haversacks     |               |
+|  [Day 8](https://adventofcode.com/2020/day/8)  |    Handheld Halting     |               |
+|  [Day 9](https://adventofcode.com/2020/day/9)  |     Encoding Error      |               |
+| [Day 10](https://adventofcode.com/2020/day/10) |      Adapter Array      |               |
+| [Day 11](https://adventofcode.com/2020/day/11) |     Seating System      |               |
+| [Day 12](https://adventofcode.com/2020/day/12) |        Rain Risk        |               |
+| [Day 13](https://adventofcode.com/2020/day/13) |     Shuttle Search      |               |
+| [Day 14](https://adventofcode.com/2020/day/14) |      Docking Data       |               |
+| [Day 15](https://adventofcode.com/2020/day/15) | Rambunctious Recitation |               |
+| [Day 16](https://adventofcode.com/2020/day/16) |   Ticket Translation    |               |
+| [Day 17](https://adventofcode.com/2020/day/17) |      Conway Cubes       |               |
+| [Day 18](https://adventofcode.com/2020/day/18) |     Operation Order     |               |
+| [Day 19](https://adventofcode.com/2020/day/19) |    Monster Messages     |               |
+| [Day 20](https://adventofcode.com/2020/day/20) |     Jurassic Jigsaw     |               |
+| [Day 21](https://adventofcode.com/2020/day/21) |   Allergen Assessment   |               |
+| [Day 22](https://adventofcode.com/2020/day/22) |       Crab Combat       |               |
+| [Day 23](https://adventofcode.com/2020/day/23) |        Crab Cups        |               |
+| [Day 24](https://adventofcode.com/2020/day/24) |      Lobby Layout       |               |
+| [Day 25](https://adventofcode.com/2020/day/25) |      Combo Breaker      |               |
 
 ## 2019
 
--- y2020/src/lib.rs
+++ y2020/src/lib.rs
@@ -2,15 +2,45 @@ pub mod day1 {
     pub type Int = u32;
 
     pub fn generator(input: &str) -> Vec<Int> {
-        std::unimplemented!();
+        input
+            .lines()
+            .map(|x| x.parse().expect("not an integer"))
+            .collect()
     }
 
     pub fn part1(input: &[Int]) -> Int {
-        std::unimplemented!();
+        //TODO is there a better way to do this
+        for left in input {
+            for right in input {
+                // skip if processing same number
+                if left == right {
+                    continue;
+                } else if left + right == 2020 {
+                    return left * right;
+                }
+            }
+        }
+
+        // else or error
+        0
     }
 
-    pub fn part2(input: &[Int]) -> usize {
-        std::unimplemented!();
+    pub fn part2(input: &[Int]) -> Int {
+        for one in input {
+            for two in input {
+                for three in input {
+                    // skip if processing same number
+                    if (one == two) || (one == three) || (two == three) {
+                        continue;
+                    } else if (one + two + three) == 2020 {
+                        return one * two * three;
+                    }
+                }
+            }
+        }
+
+        // else or error
+        0
     }
 
     #[cfg(test)]
@@ -18,35 +48,38 @@ pub mod day1 {
         use super::*;
         use std::fs::read_to_string;
 
-        const EXAMPLE: &str = "";
+        const EXAMPLE: &str = "1721
+979
+366
+299
+675
+1456";
 
         #[test]
-        #[ignore]
         fn test_part1() {
             let input = generator(EXAMPLE);
-            assert_eq!(part1(&input), 7);
+            assert_eq!(part1(&input), 514579);
         }
 
         #[test]
-        #[ignore]
         fn test_part2() {
             let input = generator(EXAMPLE);
-            assert_eq!(part2(&input), 5);
+            assert_eq!(part2(&input), 241861950);
         }
 
         #[test]
-        #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day1.txt";
+            let input_file = "../data/2020/day1.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
-            assert_eq!(part1(&input), 1448);
-            assert_eq!(part2(&input), 1471);
+            assert_eq!(part1(&input), 55776);
+            assert_eq!(part2(&input), 223162626);
         }
     }
 }
 
+/*
 pub mod day2 {
     pub type Int = u32;
 
@@ -86,7 +119,7 @@ pub mod day2 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day2.txt";
+            let input_file = "../data/2020/day2.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -135,7 +168,7 @@ pub mod day3 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day3.txt";
+            let input_file = "../data/2020/day3.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -184,7 +217,7 @@ pub mod day4 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day4.txt";
+            let input_file = "../data/2020/day4.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -233,7 +266,7 @@ pub mod day5 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day5.txt";
+            let input_file = "../data/2020/day5.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -282,7 +315,7 @@ pub mod day6 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day6.txt";
+            let input_file = "../data/2020/day6.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -331,7 +364,7 @@ pub mod day7 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day7.txt";
+            let input_file = "../data/2020/day7.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -380,7 +413,7 @@ pub mod day8 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day8.txt";
+            let input_file = "../data/2020/day8.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -429,7 +462,7 @@ pub mod day9 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day9.txt";
+            let input_file = "../data/2020/day9.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -478,7 +511,7 @@ pub mod day10 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day10.txt";
+            let input_file = "../data/2020/day10.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -527,7 +560,7 @@ pub mod day11 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day11.txt";
+            let input_file = "../data/2020/day11.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -576,7 +609,7 @@ pub mod day12 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day12.txt";
+            let input_file = "../data/2020/day12.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -625,7 +658,7 @@ pub mod day13 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day13.txt";
+            let input_file = "../data/2020/day13.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -674,7 +707,7 @@ pub mod day14 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day14.txt";
+            let input_file = "../data/2020/day14.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -723,7 +756,7 @@ pub mod day15 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day15.txt";
+            let input_file = "../data/2020/day15.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -772,7 +805,7 @@ pub mod day16 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day16.txt";
+            let input_file = "../data/2020/day16.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -821,7 +854,7 @@ pub mod day17 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day17.txt";
+            let input_file = "../data/2020/day17.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -870,7 +903,7 @@ pub mod day18 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day18.txt";
+            let input_file = "../data/2020/day18.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -919,7 +952,7 @@ pub mod day19 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day19.txt";
+            let input_file = "../data/2020/day19.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -968,7 +1001,7 @@ pub mod day20 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day20.txt";
+            let input_file = "../data/2020/day20.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -1016,7 +1049,7 @@ pub mod day21 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day21.txt";
+            let input_file = "../data/2020/day21.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -1065,7 +1098,7 @@ pub mod day22 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day22.txt";
+            let input_file = "../data/2020/day22.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -1114,7 +1147,7 @@ pub mod day23 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day23.txt";
+            let input_file = "../data/2020/day23.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -1163,7 +1196,7 @@ pub mod day24 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day24.txt";
+            let input_file = "../data/2020/day24.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -1212,7 +1245,7 @@ pub mod day25 {
         #[test]
         #[ignore]
         fn test_answers() {
-            let input_file = "../data/2021/day25.txt";
+            let input_file = "../data/2020/day25.txt";
             let input_str =
                 read_to_string(&input_file).expect(&format!("Input file {} not exist", input_file));
             let input = generator(&input_str);
@@ -1221,3 +1254,4 @@ pub mod day25 {
         }
     }
 }
+*/
