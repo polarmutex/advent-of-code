c0c56de6628919e2b45fc6ec7a2fbb64d03aa9ec
Brian Ryall <brian@brianryall.xyz>
2022-12-02 01:22:51 -0500

    feat: 2022-2

--- aoc_2022/src/day02.rs
+++ aoc_2022/src/day02.rs
@@ -5,24 +5,136 @@ day!(2, parse => part1, part2);
 //fn input_parser() -> impl Parser<char, Vec<Vec<u32>>, Error = Simple<char>> {
 //}
 
-fn parse(_input: &str) -> ParseResult<Vec<u8>> {
-    todo!()
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum RockPaperScissor {
+    Rock,
+    Paper,
+    Scissor,
 }
 
-fn part1(_input: &[u8]) -> u32 {
-    todo!()
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Outcome {
+    Lose,
+    Draw,
+    Win,
 }
 
-fn part2(_input: &[u8]) -> u32 {
-    todo!()
+use Outcome::*;
+use RockPaperScissor::*;
+
+struct Round {
+    opp: RockPaperScissor,
+    me: Outcome,
+}
+
+fn parse(input: &str) -> ParseResult<Vec<Round>> {
+    Ok(input
+        .trim()
+        .split('\n')
+        .map(|r| Round {
+            opp: match r.chars().next().unwrap() {
+                'A' => Rock,
+                'B' => Paper,
+                'C' => Scissor,
+                _ => unreachable!(),
+            },
+            me: match r.chars().nth(2).unwrap() {
+                'X' => Lose,
+                'Y' => Draw,
+                'Z' => Win,
+                _ => unreachable!(),
+            },
+        })
+        .collect())
+}
+
+fn part1(input: &[Round]) -> u32 {
+    let mut score = 0;
+    for round in input {
+        let mut round_score = 0;
+        let selection = match round.me {
+            Lose => Rock,
+            Draw => Paper,
+            Win => Scissor,
+        };
+        match round.me {
+            Lose => round_score += 1,
+            Draw => round_score += 2,
+            Win => round_score += 3,
+        }
+        if selection == Rock && round.opp == Scissor {
+            round_score += 6;
+        }
+        if selection == Scissor && round.opp == Paper {
+            round_score += 6;
+        }
+        if selection == Paper && round.opp == Rock {
+            round_score += 6;
+        }
+
+        if selection == Rock && round.opp == Rock {
+            round_score += 3;
+        }
+        if selection == Scissor && round.opp == Scissor {
+            round_score += 3;
+        }
+        if selection == Paper && round.opp == Paper {
+            round_score += 3;
+        }
+
+        score += round_score;
+    }
+    score
+}
+
+fn part2(input: &[Round]) -> u32 {
+    let mut score = 0;
+    for round in input {
+        let mut round_score = 0;
+
+        let selection = match round.me {
+            Lose => match round.opp {
+                Rock => Scissor,
+                Paper => Rock,
+                Scissor => Paper,
+            },
+            Draw => match round.opp {
+                Rock => Rock,
+                Paper => Paper,
+                Scissor => Scissor,
+            },
+            Win => match round.opp {
+                Rock => Paper,
+                Paper => Scissor,
+                Scissor => Rock,
+            },
+        };
+        match selection {
+            Rock => round_score += 1,
+            Paper => round_score += 2,
+            Scissor => round_score += 3,
+        }
+        if round.me == Win {
+            round_score += 6;
+        }
+        if round.me == Draw {
+            round_score += 3;
+        }
+
+        score += round_score;
+    }
+    score
 }
 
 tests! {
-    const _EXAMPLE: &str = "\
+    const EXAMPLE: &str = "\
+A Y
+B X
+C Z
 ";
 
-    //simple_tests!(parse, part1, part1_example_test, EXAMPLE => 0);
-    //input_tests!(YEAR, parse, part1, part1_input_test, 0);
-    //simple_tests!(parse, part2, part2_example_test, EXAMPLE => 0);
-    //input_tests!(YEAR, parse, part2, part2_input_test, 0);
+    simple_tests!(parse, part1, part1_example_test, EXAMPLE => 15); // 8 + 1 + 6
+    input_tests!(YEAR, parse, part1, part1_input_test, 11873);
+    simple_tests!(parse, part2, part2_example_test, EXAMPLE => 12);
+    input_tests!(YEAR, parse, part2, part2_input_test, 12014);
 }
