a490481292fb903785fac3fd1b709e3e7333c6a4
Brian Ryall <brian@brianryall.xyz>
2022-12-06 00:46:40 -0500

    feat: 2022-6

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 
 ## 2022
 
-![Alt](https://img.shields.io/badge/stars%20⭐10--yellow)
-![Alt](https://img.shields.io/badge/days%20completed-5-red)
+![Alt](https://img.shields.io/badge/stars%20⭐12--yellow)
+![Alt](https://img.shields.io/badge/days%20completed-6-red)
 ![Alt](https://img.shields.io/badge/rust-blue)
 
 |                      Day                       |          Name           |     Stars     | Notes |
@@ -17,7 +17,7 @@
 |  [Day 3](https://adventofcode.com/2022/day/3)  | Rucksack Reorganization | :star: :star: |
 |  [Day 4](https://adventofcode.com/2022/day/4)  |      Camp Cleanup       | :star: :star: |
 |  [Day 5](https://adventofcode.com/2022/day/5)  |      Supply Stacks      | :star: :star: |
-|  [Day 6](https://adventofcode.com/2022/day/6)  |                         |               |
+|  [Day 6](https://adventofcode.com/2022/day/6)  |     Tuning Trouble      | :star: :star: |
 |  [Day 7](https://adventofcode.com/2022/day/7)  |                         |               |
 |  [Day 8](https://adventofcode.com/2022/day/8)  |                         |               |
 |  [Day 9](https://adventofcode.com/2022/day/9)  |                         |               |
--- aoc_2022/src/day06.rs
+++ aoc_2022/src/day06.rs
@@ -1,26 +1,65 @@
 use crate::prelude::*;
+use ahash::AHashSet;
 
 day!(6, parse => part1, part2);
 
-fn parse(_input: &str) -> ParseResult<Vec<u8>> {
-    todo!()
+fn parse(input: &str) -> ParseResult<Vec<char>> {
+    let chars = input.chars().collect();
+    Ok(chars)
 }
 
-fn part1(_input: &[u8]) -> u32 {
-    todo!()
+fn solve(input: &[char], window: u32) -> u32 {
+    let mut count = window;
+    for val in input.windows(window as usize) {
+        assert!(val.len() == window as usize);
+        let mut temp = AHashSet::new();
+        val.iter().for_each(|v| {
+            temp.insert(v);
+        });
+        if temp.len() == window as usize {
+            break;
+        }
+        count += 1;
+    }
+    count
 }
 
-fn part2(_input: &[u8]) -> u32 {
-    todo!()
+fn part1(input: &[char]) -> u32 {
+    solve(input, 4)
+}
+
+fn part2(input: &[char]) -> u32 {
+    solve(input, 14)
 }
 
 tests! {
-    const _EXAMPLE: &str = "\
+    const EXAMPLE: &str = "\
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
 ";
-    //const INPUT: &str = include_str!("../../data/2022/06.txt");
+    const EXAMPLE1: &str = "\
+bvwbjplbgvbhsrlpgdmjqwftvncz
+";
+    const EXAMPLE2: &str = "\
+nppdvjthqldpwncqszvftbrmjlhg
+";
+    const EXAMPLE3: &str = "\
+nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+";
+    const EXAMPLE4: &str = "\
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+";
+    const INPUT: &str = include_str!("../../data/2022/06.txt");
 
-    //simple_tests!(parse, part1, part1_example_test, EXAMPLE => 0);
-    //simple_tests!(parse, part1, part1_input_test, INPUT => 0);
-    //simple_tests!(parse, part2, part2_example_test, EXAMPLE => 0);
-    //simple_tests!(parse, part2, part2_input_test, INPUT => 0);
+    simple_tests!(parse, part1, part1_example_test, EXAMPLE => 7);
+    simple_tests!(parse, part1, part1_example1_test, EXAMPLE1 => 5);
+    simple_tests!(parse, part1, part1_example2_test, EXAMPLE2 => 6);
+    simple_tests!(parse, part1, part1_example3_test, EXAMPLE3 => 10);
+    simple_tests!(parse, part1, part1_example4_test, EXAMPLE4 => 11);
+    simple_tests!(parse, part1, part1_input_test, INPUT => 1042);
+    simple_tests!(parse, part2, part2_example_test, EXAMPLE => 19);
+    simple_tests!(parse, part2, part2_example1_test, EXAMPLE1 => 23);
+    simple_tests!(parse, part2, part2_example2_test, EXAMPLE2 => 23);
+    simple_tests!(parse, part2, part2_example3_test, EXAMPLE3 => 29);
+    simple_tests!(parse, part2, part2_example4_test, EXAMPLE4 => 26);
+    simple_tests!(parse, part2, part2_input_test, INPUT => 2980);
 }
