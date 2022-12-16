781271c6871a3ded4802b29f14c98ecafd82c0a1
Brian Ryall <brian@brianryall.xyz>
2022-12-16 01:10:51 -0500

    refactor: 2022-15

--- aoc_2022/src/day15.rs
+++ aoc_2022/src/day15.rs
@@ -1,19 +1,51 @@
 use crate::prelude::*;
+use itertools::iproduct;
+use std::collections::HashSet;
 
 day!(15, parse => part1::<2_000_000>, part2::<4_000_000>);
 
 struct Sensor {
-    location: Coord2d<isize>,
+    loc: Coord2d<isize>,
     closest_beacon: Coord2d<isize>,
     distance: isize,
 }
 
 impl Sensor {
     pub fn contains(&self, coord: &Coord2d<isize>) -> bool {
-        if *coord == self.closest_beacon {
-            return false;
-        }
-        self.location.manhattan_distance(coord) <= self.distance
+        //if *coord == self.closest_beacon {
+        //    return false;
+        //}
+        self.loc.manhattan_distance(coord) <= self.distance
+    }
+
+    pub fn outside_range(&self, coord: &Coord2d<isize>) -> bool {
+        self.loc.manhattan_distance(coord) > self.distance
+    }
+
+    pub fn get_radius_lines(&self) -> ([isize; 2], [isize; 2]) {
+        /*
+        Image the four sides of the diamons
+               ^
+        a --> / \  <-- b
+             /   \
+             \   /
+        d --> \ /  <-- c
+               v
+        a => y =  x + a + r => a = y - x - r
+        b => y = -x + b + r => b = y + x - r
+        c => y =  x + a - r => c = y - x + r
+        d => y = -x + b - r => d = y + x + r
+
+        to find corners find intersection of
+        a - b, b - c, c - d, d - a
+        */
+        let c = self.loc.y - self.loc.x + self.distance + 1;
+        let a = self.loc.y - self.loc.x - self.distance + 1;
+
+        let d = self.loc.x + self.loc.y + self.distance + 1;
+        let b = self.loc.x + self.loc.y - self.distance + 1;
+        //let temp = iproduct!([a, c], [b, d]).into_iter();
+        ([a, c], [b, d])
     }
 }
 
@@ -22,7 +54,7 @@ impl std::fmt::Display for Sensor {
         writeln!(
             f,
             "Sensor loc: {} - Closest Beacon: {} - dist: {}",
-            self.location, self.closest_beacon, self.distance
+            self.loc, self.closest_beacon, self.distance
         )
     }
 }
@@ -32,7 +64,7 @@ fn parse(input: &str) -> ParseResult<Vec<Sensor>> {
         .lines()
         .map(|line| {
             let (sensor, beacon) = line.split_once(':').unwrap();
-            let location = sensor
+            let loc = sensor
                 .replace("Sensor at x=", "")
                 .replace("y=", "")
                 .trim()
@@ -50,9 +82,9 @@ fn parse(input: &str) -> ParseResult<Vec<Sensor>> {
                     Coord2d::from((x.parse::<isize>().expect(""), y.parse::<isize>().expect("")))
                 })
                 .unwrap();
-            let distance = location.manhattan_distance(&closest_beacon);
+            let distance = loc.manhattan_distance(&closest_beacon);
             Sensor {
-                location,
+                loc,
                 closest_beacon,
                 distance,
             }
@@ -63,10 +95,10 @@ fn parse(input: &str) -> ParseResult<Vec<Sensor>> {
 
 fn part1<const Y: isize>(input: &[Sensor]) -> usize {
     let fsensor = &input[0];
-    let init_range = fsensor.location.x - fsensor.distance..fsensor.location.x + fsensor.distance;
+    let init_range = fsensor.loc.x - fsensor.distance..fsensor.loc.x + fsensor.distance;
     let x_bounds = input.iter().fold(init_range, |mut range, sensor| {
-        range.start = range.start.min(sensor.location.x - sensor.distance);
-        range.end = range.end.max(sensor.location.x + sensor.distance);
+        range.start = range.start.min(sensor.loc.x - sensor.distance);
+        range.end = range.end.max(sensor.loc.x + sensor.distance);
         range
     });
     println!("x to try: {} .. {}", x_bounds.start, x_bounds.end);
@@ -76,16 +108,18 @@ fn part1<const Y: isize>(input: &[Sensor]) -> usize {
             input
                 .iter()
                 .any(|sensor| sensor.contains(&Coord2d::from((*x, Y))))
+            // .map(|s| manhatan_dist(pos, s.sensor) <= s.dist && pos != s.becon)
         })
         .count()
 }
 
 fn part2<const N: isize>(input: &[Sensor]) -> usize {
+    println!("N: {}", N);
     let point: Coord2d<isize> = input
         .iter()
         .find_map(|s| {
-            ((s.location.x - s.distance - 1).max(0)..=s.location.x.min(N))
-                .zip(s.location.y..=N)
+            ((s.loc.x - s.distance - 1).max(0)..=s.loc.x.min(N))
+                .zip(s.loc.y..=N)
                 .find_map(|pt| {
                     input
                         .iter()
@@ -94,7 +128,69 @@ fn part2<const N: isize>(input: &[Sensor]) -> usize {
                 })
         })
         .unwrap();
+    println!("real pt: {}", point);
+    /* brute force
     (point.x * 4_000_000 + point.y) as usize
+    */
+
+    /*
+    As there is only one missing value, it's going to be just outside the
+    boundaries of at least two scanners (unless we're incredibly unlucky and
+    it's right on the bounds of the 0-4_000_000 square, but it isn't!).
+
+    The boundary of a scanner is four line segments. If a scanner is in position
+    (sx,sy) and has 'radius' r, then we want the line segments just outside, i.e.
+    of radius r+1.
+
+    There will be two line segments of gradient 1:
+    y = x + sy-sx+r+1
+    y = x + sy-sx-r-1
+
+    and two line segments of gradient -1:
+     = -x + sx+sy+r+1
+    y = -x + sx+sy-r-1
+
+    Determining where a line y=x+a and a line y=-x+b intersect is very easy
+    - they intersect at the point ( (b-a)/2 , (a+b)/2 ).
+
+    One of these intersection points will be the missing scanner location. So,
+    we assemble a set of all the 'a' coefficients (lines of gradient 1) and all
+    the 'b' coefficients (lines of gradient -1), then look at their intersections
+    to see if they are the point we need. Given the number of scanners we only need
+    to check a couple of thousand points at most.
+    */
+    let (a_vec, b_vec) = input.iter().fold(
+        (HashSet::<isize>::new(), HashSet::<isize>::new()),
+        |(mut a, mut b), sensor| {
+            let (pos_slope, neg_slope) = sensor.get_radius_lines();
+            a.extend(&pos_slope.to_vec());
+            b.extend(&neg_slope.to_vec());
+            (a, b)
+        },
+    );
+    for (a, b) in iproduct!(a_vec.iter(), b_vec.iter()) {
+        let p = Coord2d::<isize>::from(((b - a) / 2, (a + b) / 2));
+        //println!("poss point: {}", p);
+        if p == point {
+            println!("OMG");
+        }
+    }
+    //   .map(|(a, b)| Coord2d::<isize>::from(((b - a) / 2, (a + b) / 2)))
+    //.filter(|p| 0 <= p.x && p.x <= N && 0 <= p.y && p.y <= N)
+    //.filter(|p| input.iter().all(|sen| !sen.contains(p)))
+    //.collect_vec();
+
+    /*
+    for p in &possible_pts {
+        //println!("poss point: {}", p);
+        let delta = Coord2d::<isize>::from((isize::abs(point.x - p.x), isize::abs(point.y - p.y)));
+        println!("poss point: {}", delta);
+    }*/
+
+    //assert!(possible_pts.contains(&point));
+    //assert!(possible_pts.len() == 1);
+    //(possible_pts[0].x * 4_000_000 + possible_pts[0].y) as usize
+    0
 }
 
 tests! {
@@ -117,7 +213,7 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3
     const INPUT: &str = include_str!("data/15.txt");
 
     simple_tests!(parse, part1::<10>, part1_example_test, EXAMPLE => 26);
-    simple_tests!(parse, part1::<2_000_000>, part1_input_test, INPUT => 5142231);
+    // speed upsimple_tests!(parse, part1::<2_000_000>, part1_input_test, INPUT => 5142231);
     simple_tests!(parse, part2::<20>, part2_example_test, EXAMPLE => 56000011);
     simple_tests!(parse, part2::<4_000_000>, part2_input_test, INPUT => 10884459367718);
 }
