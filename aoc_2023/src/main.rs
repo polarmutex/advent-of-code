0900791c104891f913aae27ca52aa25907230eb8
Brian Ryall <me@brianryall.xyz>
2023-12-07 16:14:28 -0500

    feat: 2023 - 7

--- aoc_2023/src/main.rs
+++ aoc_2023/src/main.rs
@@ -1,2 +1,2 @@
 #![feature(float_next_up_down)] // day06
-framework::main!(2023, day01, day02, day03, day04, day05, day06,);
+framework::main!(2023, day01, day02, day03, day04, day05, day06, day07,);
