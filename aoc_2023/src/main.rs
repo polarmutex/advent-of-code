2f7e490732bb0ae35b396c4f54d381fdbaea5bbc
Brian Ryall <me@brianryall.xyz>
2023-12-14 15:50:03 -0500

    feat: 2023 - 14

--- aoc_2023/src/main.rs
+++ aoc_2023/src/main.rs
@@ -1,5 +1,5 @@
 #![feature(float_next_up_down)] // day06
 framework::main!(
     2023, day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12,
-    day13,
+    day13, day14,
 );
